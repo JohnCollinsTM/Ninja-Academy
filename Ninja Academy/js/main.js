3d8e736f149fc0c515807ef76b88d1e1ddc16136
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-08-05 21:44:15 +0300

    Adding typescript definitions

---
+++ Ninja Academy/js/main.js
@@ -0,0 +1,20 @@
+/// <reference path="..\typings\index.d.ts" />
+
+/*globals Phaser*/
+
+var gameState = {
+    preload: function() {  
+
+    },
+
+    create: function() {
+
+    },
+
+    update: function() {
+
+    },
+};
+
+var game = new Phaser.Game(400, 490);
+
