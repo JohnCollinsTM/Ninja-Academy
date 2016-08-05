f5c2feb803ab595149124a43613d7c0ae0edaf12
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-08-05 22:10:15 +0300

    Uploading basic styles, adding the core in main.js

--- Ninja Academy/js/main.js
+++ Ninja Academy/js/main.js
@@ -3,18 +3,27 @@
 /*globals Phaser*/
 
 var gameState = {
+    //Executed at the beginning
+    //Used to load images and audio
     preload: function() {  
 
     },
 
+    //Used to set up the game, display sprites, etc.
     create: function() {
 
     },
 
+    //Called 60 times per second and contains the game's logic
+    //Used to update game states, update sprites, read keyboard and mouse clicks
     update: function() {
 
     },
 };
 
-var game = new Phaser.Game(400, 490);
+//Creates a WIDTH x HEIGHT game
+var game = new Phaser.Game(1280, 720);
 
+game.state.add('gameState', gameState);
+
+game.state.start();
